76d1d38b082e7e905c77a5cf14bf4332a3e74567
Nordin <128930417+nordin05@users.noreply.github.com>
2023-05-01 17:43:33 +0200

    Calculator works with simple 2 variable equations e.g 5*20 But 5*5*20 doesnt work yet.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function changeDisplay(value){
 let firstNumber;
 let secondNumber;
 let operation;
-let displayValue;
+let displayValue = "";
 
 document.querySelectorAll('button').forEach(btn =>{
     btn.addEventListener('click', () =>{
@@ -40,5 +40,33 @@ document.querySelectorAll('button').forEach(btn =>{
   });
 
   function calculator(buttonPressed){
+    displayValue = displayValue + buttonPressed;
+    changeDisplay(displayValue);
 
+    if (buttonPressed == "+" || buttonPressed == "-" || buttonPressed == "*" || buttonPressed == "/"){
+        operation = buttonPressed;
+        firstNumber = displayValue.replace(buttonPressed,'');
+        //displayValue = "";
+        //changeDisplay(displayValue);
+    }
+
+    else if (buttonPressed == "=" && firstNumber != null && operation != null){
+        secondNumber = displayValue.replace("=",'');
+        secondNumber = secondNumber.replace(operation,'');
+        secondNumber = secondNumber.replace(firstNumber,'');
+        displayValue = operate(operation, parseInt(firstNumber), parseInt(secondNumber));
+        changeDisplay(displayValue);
+    }
+
+    else if (buttonPressed == "Clear"){
+        firstNumber = null;
+        secondNumber = null;
+        operation = null;
+        displayValue = "";
+        changeDisplay(displayValue);
+    }
+    console.log(`firstNumber: ${firstNumber} 
+operation: ${operation} 
+secondNumber: ${secondNumber}
+displayValue: ${displayValue}`);
   }
